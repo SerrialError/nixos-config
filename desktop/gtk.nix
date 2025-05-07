7a3fbe5e0e2f6c06608d99515106bf65cb4ea90a
Serrial Error <serrialerror@outlook.com>
2025-05-06 20:27:07 -0700

    added consistent theming across qt and gtk applications

--- desktop/gtk.nix
+++ desktop/gtk.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # GTK theme configuration
@@ -14,19 +14,60 @@
     };
     theme = {
       package = pkgs.adw-gtk3;
-      name = "adw-gtk3";
+      name = "adw-gtk3-dark";
     };
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
+      gtk-theme-name = "adw-gtk3-dark";
+      gtk-icon-theme-name = "SolArc-Dark";
+      gtk-cursor-theme-name = "Bibata-Modern-Ice";
+      gtk-toolbar-style = "GTK_TOOLBAR_ICONS";
+      gtk-menu-images = true;
+      gtk-button-images = true;
     };
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = true;
+      gtk-theme-name = "adw-gtk3-dark";
+      gtk-icon-theme-name = "SolArc-Dark";
+      gtk-cursor-theme-name = "Bibata-Modern-Ice";
     };
   };
 
   # QT theme configuration
-  qt.enable = true;
-  qt.platformTheme.name = "gtk";
-  qt.style.name = "adwaita-dark";
-  qt.style.package = pkgs.adwaita-qt;
+  qt = {
+    enable = true;
+    platformTheme = {
+      name = "gtk";
+    };
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt;
+    };
+  };
+
+  # Environment variables for consistent theming
+  home.sessionVariables = {
+    GTK_THEME = "adw-gtk3-dark";
+    QT_STYLE_OVERRIDE = "adwaita-dark";
+    QT_QPA_PLATFORMTHEME = lib.mkForce "gtk";
+    XCURSOR_THEME = "Bibata-Modern-Ice";
+    XCURSOR_SIZE = "24";
+  };
+
+  # Required packages for theming
+  home.packages = with pkgs; [
+    # GTK themes
+    adw-gtk3
+    solarc-gtk-theme
+    bibata-cursors
+    
+    # Qt themes
+    adwaita-qt
+    qt5.qtbase
+    qt6.qtbase
+    
+    # Additional theming tools
+    lxappearance  # GTK theme switcher
+    libsForQt5.qt5ct  # Qt theme switcher
+  ];
 } 
