156e948916025021edd66bd1b3a58c942eddea04
Serrial Error <serrialerror@outlook.com>
2025-05-06 20:41:53 -0700

    added compositor and other QOF changes

--- wm/i3.nix
+++ wm/i3.nix
@@ -15,6 +15,13 @@ in {
         outer = 5;
       };
 
+      # Window decoration settings
+      window = {
+        titlebar = false;
+        border = 3;
+        hideEdgeBorders = "none";
+      };
+
       # Disable i3bar since we're using Polybar
       bars = [];
 
@@ -33,6 +40,53 @@ in {
         { command = "nm-applet"; }
         { command = "sleep 2 && blueman-applet"; }
         { command = "flameshot"; }
+        { command = "picom --experimental-backends"; }
+      ];
+    };
+  };
+
+  # Picom compositor configuration
+  services.picom = {
+    enable = true;
+    package = pkgs.picom;
+    fade = true;
+    fadeDelta = 5;
+    fadeSteps = [ 0.03 0.03 ];
+    shadow = true;
+    shadowOffsets = [ (-10) (-10) ];
+    shadowOpacity = 0.3;
+    settings = {
+      # Blur settings
+      blur = {
+        method = "dual_kawase";
+        background = true;
+        background-frame = true;
+        background-fixed = true;
+      };
+      # Focus settings
+      focus-exclude = [
+        "class_g = 'Rofi'"
+        "class_g = 'Polybar'"
+      ];
+      # Window type settings
+      wintypes = {
+        tooltip = { fade = true; shadow = true; opacity = 0.1; focus = true; };
+        dock = { shadow = true; };
+        dnd = { shadow = true; };
+        popup_menu = { opacity = 0.95; };
+        dropdown_menu = { opacity = 0.95; };
+      };
+      # Inactive window settings
+      inactive-opacity = 0.95;
+      inactive-dim = 0.4;
+      # Active window settings
+      active-opacity = 1.0;
+      # Blur settings for unfocused windows
+      blur-background-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+        "class_g = 'Rofi'"
+        "class_g = 'Polybar'"
       ];
     };
   };
