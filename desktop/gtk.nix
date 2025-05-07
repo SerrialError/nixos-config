6359b69bc8b628e675e0704fb5b6ab5776f6e252
Serrial Error <serrialerror@outlook.com>
2025-05-06 20:30:55 -0700

    added new icon pack and made all theming look much better

--- desktop/gtk.nix
+++ desktop/gtk.nix
@@ -5,8 +5,8 @@
   gtk = {
     enable = true;
     iconTheme = {
-      package = pkgs.solarc-gtk-theme;
-      name = "SolArc-Dark";
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
     };
     cursorTheme = {
       package = pkgs.bibata-cursors;
@@ -19,7 +19,7 @@
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
       gtk-theme-name = "adw-gtk3-dark";
-      gtk-icon-theme-name = "SolArc-Dark";
+      gtk-icon-theme-name = "Papirus-Dark";
       gtk-cursor-theme-name = "Bibata-Modern-Ice";
       gtk-toolbar-style = "GTK_TOOLBAR_ICONS";
       gtk-menu-images = true;
@@ -28,7 +28,7 @@
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = true;
       gtk-theme-name = "adw-gtk3-dark";
-      gtk-icon-theme-name = "SolArc-Dark";
+      gtk-icon-theme-name = "Papirus-Dark";
       gtk-cursor-theme-name = "Bibata-Modern-Ice";
     };
   };
@@ -52,13 +52,21 @@
     QT_QPA_PLATFORMTHEME = lib.mkForce "gtk";
     XCURSOR_THEME = "Bibata-Modern-Ice";
     XCURSOR_SIZE = "24";
+    # Icon theme settings
+    XDG_ICON_THEME = "Papirus-Dark";
+    GTK_ICON_THEME = "Papirus-Dark";
   };
 
   # Required packages for theming
   home.packages = with pkgs; [
     # GTK themes
     adw-gtk3
-    solarc-gtk-theme
+    
+    # Icon themes
+    papirus-icon-theme
+    papirus-folders
+    hicolor-icon-theme
+    adwaita-icon-theme
     bibata-cursors
     
     # Qt themes
@@ -70,4 +78,19 @@
     lxappearance  # GTK theme switcher
     libsForQt5.qt5ct  # Qt theme switcher
   ];
+
+  # Create a script to customize Papirus folder colors
+  home.file.".local/bin/customize-papirus-folders" = {
+    text = ''
+      #!/usr/bin/env bash
+      # Customize Papirus folder colors to match the theme
+      papirus-folders -C bluegrey --theme Papirus-Dark
+    '';
+    executable = true;
+  };
+
+  # Run the folder customization script when X session starts
+  xsession.initExtra = ''
+    $HOME/.local/bin/customize-papirus-folders
+  '';
 } 
