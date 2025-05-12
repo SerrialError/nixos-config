5447ea687c0070a719f861df0b24ae5278373462
Serrial Error <serrialerror@outlook.com>
2025-05-11 21:54:41 -0700

    added qemu and other programs. Almost got vex-v5-qemu working

--- configuration.nix
+++ configuration.nix
@@ -26,8 +26,7 @@
   '';
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+  nixpkgs.overlays = [ inputs.nix-minecraft.overlay (import inputs.rust-overlay) ];
 
   # Bootloader.
   boot.loader.grub.enable = true;
@@ -308,6 +307,7 @@
     wget
     alacritty
     mangohud
+    cargo
     xclip  # Add xclip for clipboard support
     rofi    # Add rofi for application launcher
     desktop-file-utils  # Add desktop-file-utils for desktop database management
@@ -315,6 +315,21 @@
     papirus-icon-theme  # Add Papirus icon theme
     gtk3  # Add GTK3 for icon support
     pulseaudio  # Add pulseaudio for pactl command
+    inputs.cargo-v5.packages.${pkgs.system}.cargo-v5-full  # Add cargo-v5
+    llvmPackages.bintools  # Add LLVM tools
+    cargo-binutils  # Add cargo-binutils
+    (pkgs.rust-bin.nightly."2025-02-18".default.override {  # Add Rust nightly
+      extensions = [ "rust-analyzer" "rust-src" "clippy" ];
+    })
+    # QEMU and virtualization tools
+    qemu_full
+    virt-manager
+    virt-viewer
+    libvirt
+    OVMF
+    swtpm
+    quickemu  # Add Quickemu for easy VM management
+    pkgs.vex-v5-simulator  # Add VEX V5 QEMU emulator
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -363,9 +378,9 @@
   };
 
   # Open ports in the firewall.
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 22 25565 8080 8443 ];
-  networking.firewall.allowedUDPPorts = [ 22 25565 8080 8443 ];
+  networking.firewall.enable = false;
+  # networking.firewall.allowedTCPPorts = [ 22 25565 8080 8443 ];
+  # networking.firewall.allowedUDPPorts = [ 22 25565 8080 8443 ];
   # Or disable the firewall altogether.
 
   # This value determines the NixOS release from which the default
@@ -376,4 +391,17 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
+  # Enable virtualization
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_full;
+        runAsRoot = true;
+      };
+    };
+  };
+
+  # Add virtualization group
+  users.extraGroups.libvirtd.members = [ "connor" ];
 }
