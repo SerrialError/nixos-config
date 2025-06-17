7dfde37570ca3da4282cc7a85013722471646c26
Serrial Error <serrialerror@outlook.com>
2025-06-17 10:25:15 -0700

    got rid of cachix

--- flake.nix
+++ flake.nix
@@ -27,9 +27,4 @@
     };
   };
 
-  # Add the features directory to the Nix store
-  nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
-  };
 }
